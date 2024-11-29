aa0286b4ba2ce4e3ed5477cfa74e2cdf034150a7
Enock97 <enockladu@hotmail.com>
2024-11-29 10:28:59 +0100

    + Added Bet API classes - Deleted template Weather API

---
+++ BetController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bobs_Racing
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BetController : ControllerBase
+    {
+        private readonly IBetRepository _betRepository;
+
+        public BetController(IBetRepository betRepository)
+        {
+            _betRepository = betRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBets()
+        {
+            var bets = await _betRepository.GetAllBetsAsync();
+            return Ok(bets);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBetById(int id)
+        {
+            var bet = await _betRepository.GetBetByIdAsync(id);
+            if (bet == null)
+            {
+                return NotFound();
+            }
+            return Ok(bet);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBet([FromBody] Bet bet)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _betRepository.CreateBetAsync(bet);
+            return CreatedAtAction(nameof(GetBetById), new { id = bet.BetID }, bet);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBet(int id, [FromBody] Bet bet)
+        {
+            if (id != bet.BetID)
+            {
+                return BadRequest("Bet ID mismatch.");
+            }
+
+            await _betRepository.UpdateBetAsync(bet);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBet(int id)
+        {
+            await _betRepository.DeleteBetAsync(id);
+            return NoContent();
+        }
+    }
+
+}
---
+++ BetRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bobs_Racing
+{
+    public class BetRepository : IBetRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BetRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Bet>> GetAllBetsAsync()
+        {
+            return await _context.Bets
+                .Include(b => b.Race)
+                .Include(b => b.Animal)
+                .ToListAsync();
+        }
+
+        public async Task<Bet> GetBetByIdAsync(int betId)
+        {
+            return await _context.Bets
+                .Include(b => b.Race)
+                .Include(b => b.Animal)
+                .FirstOrDefaultAsync(b => b.BetID == betId);
+        }
+
+        public async Task CreateBetAsync(Bet bet)
+        {
+            await _context.Bets.AddAsync(bet);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBetAsync(Bet bet)
+        {
+            _context.Bets.Update(bet);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBetAsync(int betId)
+        {
+            var bet = await _context.Bets.FindAsync(betId);
+            if (bet != null)
+            {
+                _context.Bets.Remove(bet);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+
+}
