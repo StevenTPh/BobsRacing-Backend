9fb17992893a96a99b8704d9a9508dc472b43a59
Enock97 <enockladu@hotmail.com>
2024-12-02 08:50:33 +0100

    + Oppdaterte disse filene

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,7 +1,5 @@
 ﻿using Bobs_Racing.Models;
-using Bobs_Racing.Models;
 using Microsoft.EntityFrameworkCore;
-using Bobs_Racing.Model;
 using System.Text.Json;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
@@ -19,17 +17,28 @@ namespace Bobs_Racing.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Race>()
-                .HasKey(r => r.RaceId);
+            modelBuilder.Entity<RaceAnimal>()
+                .HasKey(ra => new { ra.RaceId, ra.AnimalId });
 
-            modelBuilder.Entity<Race>()
-                .HasMany(r => r.Animals)
-                .WithOne(a => a.Race)
-                .HasForeignKey(a => a.RaceId);
+            modelBuilder.Entity<RaceAnimal>()
+                .HasOne(ra => ra.Race)
+                .WithMany(r => r.RaceAnimals)
+                .HasForeignKey(ra => ra.RaceId);
 
-            modelBuilder.Entity<Animal>()
-                .HasKey(a => a.AnimalId);
+            modelBuilder.Entity<RaceAnimal>()
+                .HasOne(ra => ra.Animal)
+                .WithMany(a => a.RaceAnimals)
+                .HasForeignKey(ra => ra.AnimalId);
+
+            // Value converter for CheckpointSpeeds
+            modelBuilder.Entity<RaceAnimal>()
+                .Property(ra => ra.CheckpointSpeeds)
+                .HasConversion(
+                    v => string.Join(",", v),         // Convert int[] to string for the database
+                    v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray() // Convert string back to int[]
+                );
         }
+
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -21,9 +21,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+//var app = builder.Build();
 
-//////////* Slik at frontenden kan sende http forespørsler (API Calls) til backenden - Enock
+////////// Slik at frontenden kan sende http forespørsler (API Calls) til backenden - Enock
 /*builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -35,8 +35,8 @@ var app = builder.Build();
 });
 
 app.UseCors();
-///////////*
-///
+*/
+
 builder.Services.AddDbContext<DbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
