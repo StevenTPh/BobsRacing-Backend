7f6d0c59b5143f9ef9ae364cfd8c891a553db26f
stp <steventphung6@gmail.com>
2024-12-03 14:35:10 +0100

    + RaceAnimal API

---
+++ Bobs-Racing/Interface/IRaceAnimalRepository.cs
@@ -0,0 +1,17 @@
+﻿using Bobs_Racing.Models;
+
+namespace Bobs_Racing.Interface
+{
+    public interface IRaceAnimalRepository
+    {
+        Task<IEnumerable<RaceAnimal>> GetAllRaceAnimalAsync();
+        Task<RaceAnimal> GetBetByIdAsync(int animalId, int raceId);
+        Task AddRaceAnimalAsync(RaceAnimal raceAnimal);
+        Task UpdateRaceAnimalAsync(RaceAnimal raceAnimal);
+        Task DeleteRaceAnimalAsync(int animalId, int raceId);
+
+        // New method to validate the composite key in RaceAnimal
+        Task<bool> ValidateAnimalAsync(int animalId);
+        Task<bool> ValidateRaceAsync(int raceId);
+    }
+}
