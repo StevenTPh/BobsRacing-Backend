cc49b103027d41e765118fe05a956727d0c40259
Enock97 <enockladu@hotmail.com>
2024-12-06 15:31:03 +0100

    + Added migration files

---
+++ Bobs-Racing/Migrations/20241206143007_InitialCreate.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bobs_Racing.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Athletes",
+                columns: table => new
+                {
+                    AthleteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LowestTime = table.Column<int>(type: "int", nullable: false),
+                    FastestTime = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Athletes", x => x.AthleteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Races",
+                columns: table => new
+                {
+                    RaceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Races", x => x.RaceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Credits = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RaceAthletes",
+                columns: table => new
+                {
+                    RaceAthleteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RaceId = table.Column<int>(type: "int", nullable: false),
+                    AthleteId = table.Column<int>(type: "int", nullable: false),
+                    FinalPosition = table.Column<int>(type: "int", nullable: false),
+                    AnimalId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RaceAthletes", x => x.RaceAthleteId);
+                    table.ForeignKey(
+                        name: "FK_RaceAthletes_Athletes_AthleteId",
+                        column: x => x.AthleteId,
+                        principalTable: "Athletes",
+                        principalColumn: "AthleteId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RaceAthletes_Races_RaceId",
+                        column: x => x.RaceId,
+                        principalTable: "Races",
+                        principalColumn: "RaceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bets",
+                columns: table => new
+                {
+                    BetId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    PotentialPayout = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    RaceAthleteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bets", x => x.BetId);
+                    table.ForeignKey(
+                        name: "FK_Bets_RaceAthletes_RaceAthleteId",
+                        column: x => x.RaceAthleteId,
+                        principalTable: "RaceAthletes",
+                        principalColumn: "RaceAthleteId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bets_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bets_RaceAthleteId",
+                table: "Bets",
+                column: "RaceAthleteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bets_UserId",
+                table: "Bets",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RaceAthletes_AthleteId",
+                table: "RaceAthletes",
+                column: "AthleteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RaceAthletes_RaceId",
+                table: "RaceAthletes",
+                column: "RaceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bets");
+
+            migrationBuilder.DropTable(
+                name: "RaceAthletes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Athletes");
+
+            migrationBuilder.DropTable(
+                name: "Races");
+        }
+    }
+}
