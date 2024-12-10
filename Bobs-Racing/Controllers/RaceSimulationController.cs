d157da6ee507b5545b8496cf90858c7bc3d0b028
Miadog7Extra <petter.karstensen@gmail.com>
2024-12-10 08:56:23 +0100

    last changes

--- Bobs-Racing/Controllers/RaceSimulationController.cs
+++ Bobs-Racing/Controllers/RaceSimulationController.cs
@@ -1,4 +1,5 @@
 ﻿using Bobs_Racing.Hubs;
+using Bobs_Racing.Interface;
 using Bobs_Racing.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -12,24 +13,48 @@ namespace Bobs_Racing.Controllers
     public class RaceSimulationController : ControllerBase
     {
         private readonly RaceSimulationService _simulationService;
-        private readonly List<Runner> _runners;
+        private readonly IAthleteRepository _athleteRepository;
+        //private readonly List<Runner> _runners;
 
         public RaceSimulationController(RaceSimulationService simulationService)
         {
             _simulationService = simulationService;
-            _runners = new List<Runner>
+            _athleteRepository = athleteRepository;
+            /*    _runners = new List<Runner>
         {
             new Runner { Name = "Runner 1", Speed = 0, Position = 0, Acceleration = 0.2, ReactionTime = 0.1 },
             new Runner { Name = "Runner 2", Speed = 0, Position = 0, Acceleration = 0.25, ReactionTime = 0.2 },
             // Add more runners if needed
-        };
+        };*/
         }
 
         [HttpPost("start")]
-        public async Task<IActionResult> StartRace()
+        public async Task<IActionResult> StartRace(CancellationToken cancellationToken)
         {
-            var cts = new CancellationTokenSource();
-            var task = _simulationService.StartRace(cts.Token);
+
+            var athletes = await _athleteRepository.GetAllAthletesAsync();
+
+            if (!athletes.Any())
+            {
+                return NotFound();
+            }
+
+            var runners = athletes.Select(a => new Runner
+            {
+                Name = a.Name,
+                Speed = 0,
+                Position = 0,
+                Acceleration = a.LowestTime,
+                ReactionTime = a.FastestTime
+            }).ToList();
+
+            // Pass runners to the simulation service
+            _simulationService.SetRunners(runners);
+
+            // Start the race
+            await _simulationService.StartRace(cancellationToken);
+            //var cts = new CancellationTokenSource();
+            //var task = _simulationService.StartRace(cts.Token);
 
             return Ok(new { message = "Race started!" });
         }
